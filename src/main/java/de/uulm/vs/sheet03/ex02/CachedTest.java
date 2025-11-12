251d357c816633f0e459fe0a6c8b14833b9b9844
thisisfel1x <felixweber271@gmail.com>
2025-11-12 18:14:06 +0100

    feat(sheet03): improve test class for CachedRMIClient

--- src/main/java/de/uulm/vs/sheet03/ex02/CachedTest.java
+++ src/main/java/de/uulm/vs/sheet03/ex02/CachedTest.java
@@ -6,52 +6,49 @@ public class CachedTest {
 
     public static void main(String[] args) {
         try {
-            System.out.println("--- Erzeuge Clients ---");
+            // Creating clients
             CachedRMIClient client1 = new CachedRMIClient("Client-1");
             CachedRMIClient client2 = new CachedRMIClient("Client-2");
 
             Thread.sleep(500);
 
-            System.out.println("\n--- Test 1: C1 schreibt 'keyA' ---");
+            System.out.println("Test 1: C1 WRITE keyA");
             client1.write("keyA", "Wert_123");
             client1.printLocalCache();
             client2.printLocalCache();
 
-            System.out.println("\n--- Test 2: C2 liest 'keyA' (Cache-Miss & Subscribe) ---");
+            System.out.println("Test 2: C2 READ keyA -> MISS & SUBSCRIBE");
             String val = client2.read("keyA");
             System.out.println("C2 hat gelesen: " + val);
             client1.printLocalCache();
             client2.printLocalCache();
 
-            System.out.println("\n--- Test 3: C1 überschreibt 'keyA' ---");
+            System.out.println("Test 3: C1 OVERWRITE keyA");
             client1.write("keyA", "Wert_456_NEU");
 
             Thread.sleep(1000);
 
-            System.out.println("\n--- Test 4: Caches prüfen nach Update ---");
+            System.out.println("Checking caches...");
             client1.printLocalCache();
             client2.printLocalCache();
 
-            System.out.println("\n--- Test 5: C1 löscht 'keyA' ---");
+            System.out.println("Test 4: C1 DELETE keyA");
             client1.remove("keyA");
 
             Thread.sleep(1000);
 
-            System.out.println("\n--- Test 6: Caches prüfen nach Löschen ---");
+            System.out.println("Checking caches...");
             client1.printLocalCache();
             client2.printLocalCache();
 
-            System.out.println("\n--- Test 7: C1 liest gelöschten Key ---");
+            System.out.println("Test 5: C1 READ Key (already deleted)");
             val = client1.read("keyA");
-            System.out.println("C1 hat gelesen: " + val);
-
-            System.out.println("\n--- Demo abgeschlossen ---");
+            System.out.println("C1 has read: " + val);
 
             System.exit(0);
 
-        } catch (RemoteException | InterruptedException e) {
-            System.err.println("Fehler im CacheTest: " + e);
-            e.printStackTrace();
+        } catch (RemoteException | InterruptedException exception) {
+            exception.printStackTrace();
         }
     }
 
